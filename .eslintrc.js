52168fd92abc3283772ea8a6df2d1a5ccbe4fdb5
CaoMeiYouRen <996881204@qq.com>
2020-12-19 19:03:12 +0800

    chore: 修改 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,9 @@
 module.exports = {
     root: true,
-    env: {
-        node: true
-    },
     extends: [
-        "eslint:recommended",
-        "@vue/typescript/recommended",
         'cmyr/vue',
     ],
-    parser: 'vue-eslint-parser',
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-        ecmaVersion: new Date().getFullYear(),
-        sourceType: 'module',
-        ecmaFeatures: {
-            modules: true,
-            jsx: true,
-        },
-    },
     rules: {
-        'vue/require-name-property': [0]
-    }
-};
+        'vue/require-name-property': [0],
+    },
+}
